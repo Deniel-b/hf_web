9c61961165beba08fe59e1cde8cc711ab27b3c40
Deniel <kuravin-denis@yandex.ru>
2020-09-10 19:31:33 +0300

    f

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@server.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
