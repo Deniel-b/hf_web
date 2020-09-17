716c234b705eecb25129df7e713ae2a0a865de6b
Deniel <kuravin-denis@yandex.ru>
2020-09-17 19:16:58 +0300

    delet etc Lines

---
+++ app.py
@@ -0,0 +1,5 @@
+from main.py import app
+
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from flask import Flask, config
+from flask import Flask, config, request
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route("/", methods=['GET'])
+@app.route("/", methods=['GET'])
 def index():
     return render_template("index.html")
-
-
-db = SQLAlchemy(app)
-app.confg
