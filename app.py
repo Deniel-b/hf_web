4ae2b75fb2f9398099df7f1a4ed82c67101e305d
Deniel <kuravin-denis@yandex.ru>
2020-09-19 17:51:07 +0300

    major changes

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
-from flask import Flask, config, request
-from flask import render_template,g
-# from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, config, request, url_for
+from flask import render_template, g
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3  
+import os
 
 app = Flask(__name__, static_folder='static', static_url_path='')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 # DATABASE = '/path/to/database.db'
 
 @app.route("/", methods=['GET'])
 def index():
     return app.send_static_file("index.html")
 
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
 
-'''def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is None:
-        db.close()'''
-
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
